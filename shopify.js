761fe6b6e5dcb470d6c780c38cad582c1729c15e
ojayii <139297476+ojayii@users.noreply.github.com>
2023-11-04 00:43:21 +0100

    Add files via upload

---
+++ shopify.js
@@ -0,0 +1,54 @@
+function toggleText() {
+            var button = document.getElementById('toggle-button');
+            var additionalText = document.getElementById('additional-text');
+
+            if (additionalText.style.display === 'none' || additionalText.style.display === '') {
+                additionalText.style.display = 'block';
+                button.innerHTML = '-';
+            } else {
+                additionalText.style.display = 'none';
+                button.innerHTML = '+';
+            }
+        }
+   
+   
+function toggleText2() {
+            var button = document.getElementById('toggle-button2');
+            var additionalText2 = document.getElementById('additional-text2');
+
+            if (additionalText2.style.display === 'none' || additionalText2.style.display === '') {
+                additionalText2.style.display = 'block';
+                button.innerHTML = '-';
+            } else {
+                additionalText2.style.display = 'none';
+                button.innerHTML = '+';
+            }
+        }
+    
+    
+function toggleText3() {
+            var button = document.getElementById('toggle-button3');
+            var additionalText3 = document.getElementById('additional-text3');
+
+            if (additionalText3.style.display === 'none' || additionalText3.style.display === '') {
+                additionalText3.style.display = 'block';
+                button.innerHTML = '-';
+            } else {
+                additionalText3.style.display = 'none';
+                button.innerHTML = '+';
+            }
+        }
+    
+    
+function toggleText4() {
+            var button = document.getElementById('toggle-button4');
+            var additionalText4 = document.getElementById('additional-text4');
+
+            if (additionalText4.style.display === 'none' || additionalText4.style.display === '') {
+                additionalText4.style.display = 'block';
+                button.innerHTML = '-';
+            } else {
+                additionalText4.style.display = 'none';
+                button.innerHTML = '+';
+            }
+        }
