3245c82d0a25c295e2f477add3a65fd42d8d06ff
Joseph Ogelenya <josephoges9@gmail.com>
2023-11-24 16:07:25 +0100

    Update shopify.js

--- shopify.js
+++ shopify.js
@@ -1,77 +1,81 @@
-function xBtn() {
-         document.getElementById('row1').style.display='none'
-         document.getElementById('topgrad').style.marginTop='0px'
-}
+let xBtn = document.getElementById('xbtn')
+let row1 = document.getElementById('row1')
+let topGrad = document.getElementById('topgrad')
+
+xBtn.addEventListener("click", function () {
+    row1.style.display = "none"
+    topGrad.style.marginTop = "0px"
+})
+
 function toggleText() {
-            var button = document.getElementById('toggle-button');
-            var additionalText = document.getElementById('additional-text');
+    var button = document.getElementById('toggle-button');
+    var additionalText = document.getElementById('additional-text');
 
-            if (additionalText.style.display === 'none' || additionalText.style.display === '') {
-                additionalText.style.display = 'block';
-                button.innerHTML = '-';
-                button.style.backgroundColor = '#F0FFF8';
-                button.style.color = 'black';
-                
-                
-            } else {
-                additionalText.style.display = 'none';
-                button.innerHTML = '+';
-                button.style.backgroundColor = 'black';
-                button.style.color = 'white';
-            }
-        }
-   
-   
+    if (additionalText.style.display === 'none' || additionalText.style.display === '') {
+        additionalText.style.display = 'block';
+        button.innerHTML = '-';
+        button.style.backgroundColor = '#F0FFF8';
+        button.style.color = 'black';
+
+
+    } else {
+        additionalText.style.display = 'none';
+        button.innerHTML = '+';
+        button.style.backgroundColor = 'black';
+        button.style.color = 'white';
+    }
+}
+
+
 function toggleText2() {
-            var button = document.getElementById('toggle-button2');
-            var additionalText2 = document.getElementById('additional-text2');
+    var button = document.getElementById('toggle-button2');
+    var additionalText2 = document.getElementById('additional-text2');
 
-            if (additionalText2.style.display === 'none' || additionalText2.style.display === '') {
-                additionalText2.style.display = 'block';
-                button.innerHTML = '-';
-                button.style.backgroundColor = '#F0FFF8';
-                button.style.color = 'black';
-            } else {
-                additionalText2.style.display = 'none';
-                button.innerHTML = '+';
-                button.style.backgroundColor = 'black';
-                button.style.color = 'white';
-            }
-        }
-    
-    
+    if (additionalText2.style.display === 'none' || additionalText2.style.display === '') {
+        additionalText2.style.display = 'block';
+        button.innerHTML = '-';
+        button.style.backgroundColor = '#F0FFF8';
+        button.style.color = 'black';
+    } else {
+        additionalText2.style.display = 'none';
+        button.innerHTML = '+';
+        button.style.backgroundColor = 'black';
+        button.style.color = 'white';
+    }
+}
+
+
 function toggleText3() {
-            var button = document.getElementById('toggle-button3');
-            var additionalText3 = document.getElementById('additional-text3');
+    var button = document.getElementById('toggle-button3');
+    var additionalText3 = document.getElementById('additional-text3');
 
-            if (additionalText3.style.display === 'none' || additionalText3.style.display === '') {
-                additionalText3.style.display = 'block';
-                button.innerHTML = '-';
-                button.style.backgroundColor = '#F0FFF8';
-                button.style.color = 'black';
-            } else {
-                additionalText3.style.display = 'none';
-                button.innerHTML = '+';
-                button.style.backgroundColor = 'black';
-                button.style.color = 'white';
-            }
-        }
-    
-    
+    if (additionalText3.style.display === 'none' || additionalText3.style.display === '') {
+        additionalText3.style.display = 'block';
+        button.innerHTML = '-';
+        button.style.backgroundColor = '#F0FFF8';
+        button.style.color = 'black';
+    } else {
+        additionalText3.style.display = 'none';
+        button.innerHTML = '+';
+        button.style.backgroundColor = 'black';
+        button.style.color = 'white';
+    }
+}
+
+
 function toggleText4() {
-            var button = document.getElementById('toggle-button4');
-            var additionalText4 = document.getElementById('additional-text4');
+    var button = document.getElementById('toggle-button4');
+    var additionalText4 = document.getElementById('additional-text4');
 
-            if (additionalText4.style.display === 'none' || additionalText4.style.display === '') {
-                additionalText4.style.display = 'block';
-                button.innerHTML = '-';
-                button.style.backgroundColor = '#F0FFF8';
-                button.style.color = 'black';
-            } else {
-                additionalText4.style.display = 'none';
-                button.innerHTML = '+';
-                button.style.backgroundColor = 'black';
-                button.style.color = 'white';
-            }
-                        }
-                            
+    if (additionalText4.style.display === 'none' || additionalText4.style.display === '') {
+        additionalText4.style.display = 'block';
+        button.innerHTML = '-';
+        button.style.backgroundColor = '#F0FFF8';
+        button.style.color = 'black';
+    } else {
+        additionalText4.style.display = 'none';
+        button.innerHTML = '+';
+        button.style.backgroundColor = 'black';
+        button.style.color = 'white';
+    }
+}
